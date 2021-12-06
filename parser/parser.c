ea86df9dc359e2993faf53be5f79f9e4fa5582c3
Thao Nessround <tnessrou@ge-b3.kzn.21-school.ru>
2021-12-06 19:36:29 +0300

    added double quotes, fixed leaks

--- parser/parser.c
+++ parser/parser.c
@@ -1,6 +1,12 @@
 #include "libft.h"
 #include <stdio.h>
 
+void	cleaning(char **data)
+{
+	free(*data);
+	*data = NULL;
+}
+
 char	*slash_in_quotes(char *str)
 {
 	int		i;
@@ -36,18 +42,20 @@ char	*quotes(char *str, int *i)
 	char	*after;
 
 	help = *i;
-	while (str[++*i] && str[*i] != '\'')
+	(*i)++;
+	while (str[*i] && str[*i] != '\'')
+		(*i)++;
 	before = ft_substr(str, 0, help);
 	now = ft_substr(str, help + 1, *i - help - 1);
-	now = slash_in_quotes(now);
+	// now = slash_in_quotes(now);
 	after = ft_strdup(str + *i + 1);
-	free(str);
+	cleaning(&str);
 	str = ft_strjoin(before, now);
-	free(before);
-	free(now);
+	cleaning(&before);
+	cleaning(&now);
 	now = ft_strjoin(str, after);
-	free(str);
-	free(after);
+	cleaning(&str);
+	cleaning(&after);
 	*i = *i - 2;
 	return (now);
 }
@@ -59,24 +67,48 @@ char	*slash(char *str, int *i)
 
 	before = ft_substr(str, 0, *i);
 	after = ft_strdup(str + *i);
-	free(str);
+	cleaning(&str);
 	str = ft_strjoin(before, after);
-	free(before);
-	free(after);
+	cleaning(&before);
+	cleaning(&after);
 	return (str);
 }
 
 char	*double_quotes(char *str, int *i)
 {
+	int		help;
+	char	*before;
+	char	*now;
+	char	*after;
 
+	help = *i;
+	(*i)++;
+	while (str[*i] && str[*i] != '"')
+	{
+		if (str[*i] == '\\' && (str[*i + 1] == '"' || str[*i + 1] == '$'))
+			str = slash(str, i);
+		(*i)++;
+	}
+	before = ft_substr(str, 0, help);
+	now = ft_substr(str, help + 1, *i - help - 1);
+	after = ft_strdup(str + *i + 1);
+	cleaning(&str);
+	str = ft_strjoin(before, now);
+	cleaning(&before);
+	cleaning(&now);
+	now = ft_strjoin(str, after);
+	cleaning(&str);
+	cleaning(&after);
+	*i = *i - 2;
+	return (now);
 }
 
 void	parser(char *str)
 {
 	int	i;
 
-	i = -1;
-	while (str[++i])
+	i = 0;
+	while (str[i])
 	{
 		if (str[i] == '\'')
 			str = quotes(str, &i);
@@ -84,15 +116,20 @@ void	parser(char *str)
 			str = slash(str, &i);
 		if (str[i] == '"')
 			str = double_quotes(str, &i);
+		i++;
 	}
 	printf("%s\n", str);
+	cleaning(&str);
 }
 
 int	main(void)
 {
 	char	*str;
 
-	str = ft_strdup("Hello s\\'s world \\fdf\\\\\\df' nice morning    'jfklasjlkasjfsdao4uj4u845495'4394r84___  ' dsf");
+	str = ft_strdup("He\"l\\$l\"o s\\'s world \\fdf\\\\\\df' nice morning    'jfklasjlkasj\"fsdao4uj4u84\"5495'4394r84___  ' dsf");
+	// str = ft_strdup("hi 'ret\\ards'!");
 	parser(str);
+	// cleaning(&str);
+	while(1);
 	return (0);
 }
